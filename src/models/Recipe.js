131aefd3e95e54a029b80370d1232b1f78b86bb8
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-10-01 12:55:13 +0100

    Deconnected database after used

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -1,59 +1,89 @@
 import pool from '../config/db.js';
+
 class Recipe {
   static async checkRecipe(titre) {
     const connection = await pool.getConnection();
-    const [result] = await connection.execute(
-      'select * from recettes where titre = ?',
-      [titre]
-    );
-    return result.length;
+    try {
+      const [result] = await connection.execute(
+        'select * from recettes where titre = ?',
+        [titre]
+      );
+      return result.length;
+    } finally {
+      connection.release();
+    }
   }
 
   static async getId(id) {
     const connection = await pool.getConnection();
-    const [result] = await connection.execute(
-      'select * from recettes where id = ?',
-      [id]
-    );
-    return result.length;
+    try {
+      const [result] = await connection.execute(
+        'select * from recettes where id = ?',
+        [id]
+      );
+      return result.length;
+    } finally {
+      connection.release();
+    }
   }
 
   static async getRecipeById(id) {
     const connection = await pool.getConnection();
-    const [result] = await connection.execute(
-      'select * from recettes where id = ?',
-      [id]
-    );
-    return result;
+    try {
+      const [result] = await connection.execute(
+        'select * from recettes where id = ?',
+        [id]
+      );
+      return result;
+    } finally {
+      connection.release();
+    }
   }
 
   static async getRecipes() {
     const connection = await pool.getConnection();
-    const [result] = await connection.execute('select * from recettes');
-    return result;
+    try {
+      const [result] = await connection.execute('select * from recettes');
+      return result;
+    } finally {
+      connection.release();
+    }
   }
 
   static async createRecipe(titre, ingredients, type) {
     const connection = await pool.getConnection();
-    await connection.execute(
-      'INSERT INTO recettes(titre, ingredients, type) VALUES (?, ?, ?)',
-      [titre, ingredients, type]
-    );
-    return true;
+    try {
+      await connection.execute(
+        'INSERT INTO recettes(titre, ingredients, type) VALUES (?, ?, ?)',
+        [titre, ingredients, type]
+      );
+      return true;
+    } finally {
+      connection.release();
+    }
   }
 
   static async delRecipe(id) {
     const connection = await pool.getConnection();
-    await connection.execute('delete from recettes where id = ?', [id]);
-    return true;
+    try {
+      await connection.execute('delete from recettes where id = ?', [id]);
+      return true;
+    } finally {
+      connection.release();
+    }
   }
+
   static async editRecipe(id, titre, ingredients, type) {
     const connection = await pool.getConnection();
-    await connection.execute(
-      'update recettes set titre = ?, ingredients = ?, type = ? where id = ?',
-      [titre, ingredients, type, id]
-    );
-    return true;
+    try {
+      await connection.execute(
+        'update recettes set titre = ?, ingredients = ?, type = ? where id = ?',
+        [titre, ingredients, type, id]
+      );
+      return true;
+    } finally {
+      connection.release();
+    }
   }
 }
 
