60bc15d098f123e930a12a9ad6afc938963d6642
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-10-01 12:44:59 +0100

    Updated script file

--- assets/script.sql
+++ assets/script.sql
@@ -1,14 +1,15 @@
 drop database if exists gestion_recettes;
-create database gestion_recettes;
+create database if not exists gestion_recettes;
 
 use gestion_recettes;
 
 create table recettes(
    id int auto_increment,
-   titre varchar(50) not null,
-   ingredients varchar(255) not null,
-   type varchar(100) not null,
-   constraint pk_recette primary key(id)
+   titre varchar(100) not null,
+   ingredients text not null,
+   type varchar(50) not null,
+   constraint pk_recette primary key(id),
+   constraint ck_type check(type = 'Entrée' or type = 'Dessert' or type = 'Plat')
 );
 
 INSERT INTO recettes (titre, ingredients, type)
